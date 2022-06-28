e5ef6c857e7250ae01d20d192428ef2601faea9c
agustinespildora <jaespildora@uc.cl>
2022-06-28 14:57:31 -0400

    final commit

--- pages/pokemons/index.tsx
+++ pages/pokemons/index.tsx
@@ -18,7 +18,44 @@ export default function PokemonIndex({
   const [offset, setOffset] :[number, Dispatch<SetStateAction<number>>] = useState(12)
   const [pokemons, setPokemons] :[Pokemon[], Dispatch<SetStateAction<Pokemon[]>>] = useState(initial_pokemons)
 
-
+  const spanishTypes = (type) => {
+    switch (type) {
+      case "water":
+        return "Agua"
+      case "fire":
+        return "Fuego"
+      case "grass":
+        return "Planta"
+      case "poison":
+        return "Veneno"
+      case "bug":
+        return "Bicho"
+      case "normal":
+        return "Normal"
+      case "electric":
+        return "Eléctrico"
+      case "flying":
+        return "Volador"
+      case "ground":
+        return "Tierra"
+      case "fairy":
+        return "Hada"  
+      case "fighting":
+        return "Luchador"
+      case "psychic":
+        return "Psíquico"
+      case "rock":
+        return "Roca"
+      case "steel":
+        return "Acero"
+      case "ice":
+        return "Hielo"
+      case "ghost":
+        return "Fantasma"
+      case "dragon":
+        return "Dragón"
+      }
+  }
   const loadMorePokemons = async() => {
     const new_offset :number = offset + page_size
     const new_initial_pokemons :Pokemon[] = await getPokemons(page_size, offset)
@@ -36,7 +73,7 @@ export default function PokemonIndex({
             <strong >{name.charAt(0).toUpperCase() + name.slice(1)}</strong>
             <ul className={utilStyles.typeColumns}>
               {types.map(({type}) => (
-                <li className={utilStyles.typeTag}>{type.name}</li>
+                <li className={`${utilStyles[type.name]} ${utilStyles.typeTag}`}>{spanishTypes(type.name)}</li>
                 ))
               }
             </ul>
